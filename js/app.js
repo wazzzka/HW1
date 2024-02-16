32d02f316a94b223ae6d75fe87806c70b898ee7e
wazzzka <v.shnurenko@gmail.com>
2024-02-16 12:53:38 +0200

    add HW task

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*
+Треба реалізувати просту програму з наступними умовами:
+
+За допомогою prompt запитати ім'я користувача.
+За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+*/
+
+let name = prompt('Greetings! What is your name?');
+alert('Hello, ' + name + ' How are you?');
